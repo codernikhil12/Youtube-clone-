b5983da86ff1c4319e5ff50979003c0962694a5a
codernikhil12 <108299232+codernikhil12@users.noreply.github.com>
2024-01-12 12:48:16 +0530

    theory of subscription schema note

--- src/models/subsciption.model.js
+++ src/models/subsciption.model.js
@@ -12,3 +12,12 @@ const subscriptionSchema = new Schema({
 }, {timestamps: true})
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
+
+
+// amera jakhan kno channal ke subscribe korbo takhan database a new kore akta document create hbe  
+
+// subscibers found 
+// amader  channal find korte hbe jader name same and segulo ke count korte hbe 
+
+// akta user kon kon channal subscribed kore seta found korte hbe (channal found)
+// akjon subscribers je channal gulo subscribed kore rekheche segulo find korte hbe and value gulo sum korte hbe 
